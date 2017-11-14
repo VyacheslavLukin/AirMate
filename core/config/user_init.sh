a7582f5bef96878540286580a5d222fe5a4d45f3
VyacheslavLukin <v.s.lukin@gmail.com>
2017-11-14 13:41:51 +0100

    mysql is working

--- core/config/user_init.sh
+++ core/config/user_init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mysql -u root -pangelhack -e "GRANT ALL ON *.* TO innosoft@%;"
-mysql -u root -pangelhack -e "FLUSH PRIVILEGES;"
+mysql -u root -pinnosoft -e "GRANT ALL ON *.* TO 'innosoft'@'%';"
+mysql -u root -pinnosoft -e "FLUSH PRIVILEGES;"
