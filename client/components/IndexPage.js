b6826e87b43eda0a8b5f89aaa9c1f1fd75d6820a
Maksim <siriuz1g@gmail.com>
2017-11-14 23:58:05 +0300

    Added data and correct data presentation

--- client/components/IndexPage.js
+++ client/components/IndexPage.js
@@ -1,31 +1,24 @@
 import React from "react";
-import HeatmapOverlay from "leaflet-heatmap"
+import HeatmapOverlay from "leaflet-heatmap";
 import {makeApiGet} from "../common/Api";
 
 export default class IndexPage extends React.Component {
-    componentDidMount() {
-        makeApiGet('/api/getData').then((data) => {
+    constructor(state, props) {
+        super(state, props);
 
-        });
-        // Heatmap
-        let testData = {
-                max: 8,
-                data: [
-                    {
-                        lat: 50.7343800,
-                        lng: 7.0954900,
-                        count: 3
-                    },
-                    {
-                        lat: 50.7343800,
-                        lng: 7.0854900,
-                        count: 1
-                    }
-                ]
-            }
-        ;
+        this.state = {
+            max: 0,
+            min: -100,
+            points: []
+        };
 
-        let baseLayer = L.tileLayer(
+        this.baseLayer;
+        this.heatmapLayer;
+        this.map;
+    }
+
+    componentWillMount() {
+        this.baseLayer = L.tileLayer(
             'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
                 attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
                 maxZoom: 18,
@@ -35,36 +28,61 @@ export default class IndexPage extends React.Component {
         );
 
         let cfg = {
-            // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-            // if scaleRadius is false it will be the constant radius used in pixels
-            "radius": 0.01,
-            "maxOpacity": .8,
-            // scales the radius based on map zoom
-            "scaleRadius": true,
-            // if set to false the heatmap uses the global maximum for colorization
-            // if activated: uses the data maximum within the current map boundaries
-            //   (there will always be a red spot with useLocalExtremas true)
-            "useLocalExtrema": true,
-            // which field name in your data represents the latitude - default "lat"
+            radius: 0.1,
+            maxOpacity: .8,
+            scaleRadius: true,
+            useLocalExtrema: false,
             latField: 'lat',
-            // which field name in your data represents the longitude - default "lng"
             lngField: 'lng',
-            // which field name in your data represents the data value - default "value"
             valueField: 'count'
         };
 
-        let heatmapLayer = new HeatmapOverlay(cfg);
+        this.heatmapLayer = new HeatmapOverlay(cfg);
+    }
 
-        let map = new L.Map('map', {
+    componentDidMount() {
+        this.map = new L.Map('map', {
             center: new L.LatLng(50.7343800, 7.0954900),
-            zoom: 13,
-            layers: [baseLayer, heatmapLayer]
+            zoom: 5,
+            layers: [this.baseLayer, this.heatmapLayer]
         });
 
-        heatmapLayer.setData(testData);
+        makeApiGet('/api/getData').then((data) => {
+            let min = this.state.min;
+            let max = this.state.max;
+            const points = data.results.map(entry => {
+                if (entry.value > max) {
+                    max = entry.value;
+                } else if (entry.value < min ) {
+                    min = entry.value;
+                }
+
+                return {
+                    lat: entry.coordinates.latitude,
+                    lng: entry.coordinates.longitude,
+                    count: entry.value
+                }
+            });
+
+            this.setState(
+                Object.assign({}, {
+                    min: min,
+                    max: max,
+                    points: points
+                })
+            );
+        });
     }
 
     render() {
+        let data = {
+            max: this.state.max,
+            min: this.state.min,
+            data: this.state.points
+        };
+
+        this.heatmapLayer.setData(data);
+
         return (
             <div>
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
