59ef08ef40e07315483eedbb97fd1973f137f41b
Maksim Skorikov <siriuz1g@gmail.com>
2018-07-04 18:32:18 +0300

    Updated in correctly :)

---
+++ public/dist/0.88d2c2833cd9ce37818c.hot-update.js
@@ -0,0 +1,10 @@
+webpackHotUpdate(0,{
+
+/***/ 353:
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: SyntaxError: Unexpected token, expected ; (114:18)\n\n\u001b[0m \u001b[90m 112 | \u001b[39m      {\n \u001b[90m 113 | \u001b[39m          longitude\u001b[33m:\u001b[39m \u001b[35m55.7517163\u001b[39m\u001b[33m,\u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 114 | \u001b[39m          latitude\u001b[33m:\u001b[39m \u001b[35m48.747309\u001b[39m\u001b[33m,\u001b[39m\n \u001b[90m     | \u001b[39m                  \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 115 | \u001b[39m          data\u001b[33m:\u001b[39m [\n \u001b[90m 116 | \u001b[39m              transaction\u001b[33m:\u001b[39m \n \u001b[90m 117 | \u001b[39m              timestamp\u001b[33m:\u001b[39m now()\u001b[33m,\u001b[39m\u001b[0m\n");
+
+/***/ })
+
+})
---
+++ public/dist/0.d959ab07351c54da6735.hot-update.js
@@ -0,0 +1,112 @@
+webpackHotUpdate(0,{
+
+/***/ 104:
+false,
+
+/***/ 170:
+false,
+
+/***/ 171:
+false,
+
+/***/ 172:
+false,
+
+/***/ 173:
+false,
+
+/***/ 174:
+false,
+
+/***/ 175:
+false,
+
+/***/ 176:
+false,
+
+/***/ 177:
+false,
+
+/***/ 353:
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: SyntaxError: Unexpected token, expected ; (114:18)\n\n\u001b[0m \u001b[90m 112 | \u001b[39m      {\n \u001b[90m 113 | \u001b[39m          longitude\u001b[33m:\u001b[39m \u001b[32m\"longitude\"\u001b[39m\u001b[33m,\u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 114 | \u001b[39m          latitude\u001b[33m:\u001b[39m \u001b[32m\"latitude\"\u001b[39m\u001b[33m,\u001b[39m\n \u001b[90m     | \u001b[39m                  \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 115 | \u001b[39m          data\u001b[33m:\u001b[39m [\n \u001b[90m 116 | \u001b[39m\n \u001b[90m 117 | \u001b[39m      ]\u001b[0m\n");
+
+/***/ }),
+
+/***/ 354:
+false,
+
+/***/ 355:
+false,
+
+/***/ 356:
+false,
+
+/***/ 357:
+false,
+
+/***/ 358:
+false,
+
+/***/ 359:
+false,
+
+/***/ 360:
+false,
+
+/***/ 361:
+false,
+
+/***/ 362:
+false,
+
+/***/ 363:
+false,
+
+/***/ 364:
+false,
+
+/***/ 365:
+false,
+
+/***/ 366:
+false,
+
+/***/ 367:
+false,
+
+/***/ 368:
+false,
+
+/***/ 369:
+false,
+
+/***/ 370:
+false,
+
+/***/ 371:
+false,
+
+/***/ 372:
+false,
+
+/***/ 373:
+false,
+
+/***/ 374:
+false,
+
+/***/ 375:
+false,
+
+/***/ 376:
+false,
+
+/***/ 377:
+false,
+
+/***/ 378:
+false
+
+})
