67bd9e89cd60146a63f3b936913184f0f28beb8e
VyacheslavLukin <v.lukin@innosoft.pro>
2018-10-20 22:51:32 +0300

    More debug

--- data_layer/openaq_watchdog.py
+++ data_layer/openaq_watchdog.py
@@ -110,7 +110,9 @@ if __name__ == '__main__':
         with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'third_party', 'data_src.json'), 'r') as src:
             country_city = json.load(src)
 
+        print(country_city)
         for sCountry, lCities in country_city:
+            print(sCountry, lCities)
             for sCity in lCities:
                 print(sCity, lCities)
                 city_data = get_latest_data(country=sCountry, city=sCity)
