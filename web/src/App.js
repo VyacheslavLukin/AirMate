c879baed868d65f0843c1272c9310f0f56378bbb
reshreshus <reshreshus@gmail.com>
2019-06-02 16:56:01 +0300

    added a crappy heatmap on 'o3'

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
 import ReactMapGL, {Marker, Popup} from 'react-map-gl';
 import './App.css';
-import axios from 'axios'
+import axios from 'axios';
+
+
+const HEATMAP_SOURCE_ID = 'o3';
 
 export default class App extends Component {
 
@@ -12,6 +15,8 @@ export default class App extends Component {
 
     this.setStartingViewport = this.setStartingViewport.bind(this);
     this.handleLocation = this.handleLocation.bind(this);
+
+    this._onMapLoad = this._onMapLoad.bind(this);
 
     this.handleLocation();
   }
@@ -121,7 +126,6 @@ export default class App extends Component {
     }
     
     let data = this.state.selectedStation;
-    // console.log('data.measurements', data.measurements);
 
     let measurements = []
     if (data.measurements){
@@ -147,43 +151,145 @@ export default class App extends Component {
     return this._mapRef.current ? this._mapRef.current.getMap() : null;
   };  
   
-  // _onMapLoad = () => {
-  //   const map = this._getMap();
-  //   map.addSource(HEATMAP_SOURCE_ID, {type: 'geojson', data: response});
-  //   map.addLayer(this._mkHeatmapLayer('heatmap-layer', HEATMAP_SOURCE_ID));
-  // }
+  _onMapLoad = () => {
+    console.log('_onMapLoad');
+    const map = this._getMap();
+    let promises = []
+    let stations = this.state.stations;
+    for(let i = 0; i < stations.length; i++){
+      //TODO: this is just for experimenting
+      // in future, of course, a signle request will be used
+      let featurePromise = this.getStationInfoById(stations[i].id);
+      promises.push(featurePromise);
+    }
+
+    Promise.all(promises.map(p => p.catch(e => e))).then(
+      results => {
+        let features = []
+        for (let i = 0; i < results.length; i++){
+          if (results[i] && results[i].data && results[i].data.measurements){
+            console.log('results[i].data', results[i].data);
+            let measurements = results[i].data.measurements;
+            for (let j = 0; j < measurements.length; j++) {
+              if (measurements[j].parameter === "o3") {
+                let stationGeoFeature =  {
+                  "type": "Feature",
+                  "properties": {
+                    "paramater": measurements[j].value
+                  },
+                  "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                      stations[i].longitude,
+                      stations[i].latitude
+                      
+                    ]
+                  }
+                };
+                features.push(stationGeoFeature);
+              }
+            }
+          }
+        }
+        console.log('features', features);
+        let data = {
+          "type": "FeatureCollection",
+          "features": features
+        }
+
+        console.log('data', data);
+        map.addSource(HEATMAP_SOURCE_ID, {type: 'geojson', data: data});
+        map.addLayer(this._mkHeatmapLayer('heatmap-layer', HEATMAP_SOURCE_ID));
+      }); 
+  }
+
+  // test mapbox heatmap on "o3"
+  _mkHeatmapLayer = (id, source) => {
+    // const MAX_ZOOM_LEVEL = 18;
+    return {
+      id,
+      type: 'heatmap',
+      source,
+      maxzoom: 15,
+      paint: {
+        // increase weight as diameter breast height increases
+        'heatmap-weight': {
+          property: 'paramater',
+          type: 'exponential',
+          stops: [
+            [1, 0],
+            [62, 1]
+          ]
+        },
+        // increase intensity as zoom level increases
+        'heatmap-intensity': {
+          stops: [
+            [11, 1],
+            [15, 3]
+          ]
+        },
+        // assign color values be applied to points depending on their density
+        'heatmap-color': [
+          'interpolate',
+          ['linear'],
+          ['heatmap-density'],
+          0, 'rgba(236,222,239,0)',
+          0.2, 'rgb(208,209,230)',
+          0.4, 'rgb(166,189,219)',
+          0.6, 'rgb(103,169,207)',
+          0.8, 'rgb(28,144,153)'
+        ],
+        // increase radius as zoom increases
+        'heatmap-radius': {
+          stops: [
+            [11, 15],
+            [15, 20]
+          ]
+        },
+        // decrease opacity to transition into the circle layer
+        'heatmap-opacity': {
+          default: 1,
+          stops: [
+            [14, 1],
+            [15, 0]
+          ]
+        },
+      }
+    };
+  }
   
   
-  render () {
-    
+  render () { 
     return (
       <div>
         <ReactMapGL
+          ref={this._mapRef}
           {...this.state.viewport}
           mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
           mapStyle="mapbox://styles/reshreshus/cjwamfl3205ry1cpptvzeyq1e"
           onViewportChange={this._onViewportChange}
           // onLoad={this._onMapLoad}
         >
+          <button onClick={this._onMapLoad}> test heatmap on o3 </button>
           {this.state.stations.map(station => (
-            <Marker
-              key={station.id}
-              latitude={station.latitude}
-              longitude={station.longitude}
-            >
-              <button
-                className="marker-btn"
-                onClick={e => {
-                  e.preventDefault();
-                  // set selected station or close it if clicked for the 2nd time
-                  (this.state.selectedStation && station.id===this.state.selectedStation.id) 
-                    ? this.setSelectedStation(null) : this.setSelectedStation(station)
-                }}
+              <Marker
+                key={station.id}
+                latitude={station.latitude}
+                longitude={station.longitude}
               >
-                <img src="img/marker-icon.png" alt="marker icon" />
-              </button>
-            </Marker>
-          ))}
+                <button
+                  className="marker-btn"
+                  onClick={e => {
+                    e.preventDefault();
+                    // set selected station or close it if clicked for the 2nd time
+                    (this.state.selectedStation && station.id===this.state.selectedStation.id) 
+                      ? this.setSelectedStation(null) : this.setSelectedStation(station)
+                  }}
+                >
+                  <img src="img/marker-icon.png" alt="marker icon" />
+                </button>
+              </Marker>
+            ))}
   
           {this.state.selectedStation ? (
             
