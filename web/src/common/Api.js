3228f72aaee78f4c6f5a7b49282be7aac9015df9
reshreshus <reshreshus@gmail.com>
2019-06-20 18:11:36 +0300

    add crappy graphs (HardLight00 + reshreshus)

--- web/src/common/Api.js
+++ web/src/common/Api.js
@@ -2,19 +2,36 @@ import axios from 'axios';
 
 const URL = process.env.REACT_APP_API_URL;
 
-export const getStationInfoById = (id) => {
-    // id = id.replace('/','%2F');
-    return axios.get(`${URL}/get_station_data/${id}`);
-}
+export const getStationInfoById = id => {
+  // id = id.replace('/','%2F');
+  return axios.get(`${URL}/get_station_data/${id}`);
+};
 
 export const getStationsList = () => {
-    return axios.get(`${URL}/get_stations_list`);
-}
+  return axios.get(`${URL}/get_stations_list`);
+};
 
-export const getMeasurementsFromAllStations = (parameter) =>{
-    return axios.get(`${URL}/stations/${parameter}`);
-}
+export const getMeasurementsFromAllStations = parameter => {
+  return axios.get(`${URL}/stations/${parameter}`);
+};
 
 export const getParametersList = () => {
-    return axios.get(`${URL}/get_params_list`);
-}
+  return axios.get(`${URL}/get_params_list`);
+};
+
+export const getNearestStationData = (latitude, longitude) => {
+  return axios.get(`${URL}/get_nearest_station_data/${latitude}/${longitude}`);
+};
+
+export const getStationHistory = stationId => {
+    
+  return axios.get(`${URL}/get_station_history/${stationId}`);
+};
+
+export const getDataByDate = date => {
+  return axios.get(`${URL}/get_data_by_date/${date}`);
+};
+
+export const getStationAQI = stationId => {
+  return axios.get(`${URL}/get_station_aqi/${stationId}`);
+};
