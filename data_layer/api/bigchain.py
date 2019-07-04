ae287e3cc40655d2018aa337d2b34c0a819cc5c0
Genvekt <e.kivotova@gmail.com>
2019-07-04 16:22:38 +0300

    moved methods for fetching data from bigchain db to bigchain.py

--- data_layer/api/bigchain.py
+++ data_layer/api/bigchain.py
@@ -1,7 +1,11 @@
 from bigchaindb_driver import BigchainDB
 from bigchaindb_driver.crypto import generate_keypair
 
+
 from .config import BIGCHAIN_URL
+from .aqi import *
+import json
+import datetime
 
 
 class BigchainHelper:
@@ -32,9 +36,39 @@ class BigchainHelper:
 
     def search(self, string):
         return self.bdb.assets.get(search=string)
+    
+    def filter_by_id(self, id):
+        history = self.bdb.assets.get(search=id)
+        return  [
+            json.loads(record['data']['station_data'])
+            for record in history
+            if json.loads(record['data']['station_data'])['location'] == id
+        ]
 
+    def filter_data(self, id, aqi = False, parameters = [], date = {}, limit = -1):
+        raw_data = self.filter_by_id(id)
+        data = []
+        for record in raw_data:
+            measurements = [
+                measurement
+                for measurement in record['measurements']
+                if ((measurement['parameter'] in parameters or len(parameters) == 0 ) and (len(date) == 0 or
+                   (datetime.datetime.strptime(date['from'], '%Y-%m-%dT%H:%M:%S.%fZ') <= datetime.datetime.strptime(measurement['lastUpdated'], '%Y-%m-%dT%H:%M:%S.%fZ') and
+                   datetime.datetime.strptime(measurement['lastUpdated'], '%Y-%m-%dT%H:%M:%S.%fZ') <= datetime.datetime.strptime(date['to'], '%Y-%m-%dT%H:%M:%S.%fZ'))))
+            ]
+            if len(measurements) > 0:
+                element = {
+                        'measurements':measurements
+                    }
+                if aqi:
+                    element['aqi'] = get_aqi_of_station(measurements)
+                    
+                data.append(element)
+        return data
+            
 
 bdb = BigchainDB(BIGCHAIN_URL,
                  headers={'app_id': 'openaq_id', 'app_key': 'openaq_key'})
 bdb_helper = BigchainHelper(bdb)
 airmate_keys = generate_keypair()
+
