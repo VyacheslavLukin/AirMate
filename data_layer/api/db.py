dc32b314f4d34fc136054dd46f5620d9623091ff
Genvekt <e.kivotova@gmail.com>
2019-07-04 16:23:36 +0300

    moved methods for filtering postgres db to db.py

--- data_layer/api/db.py
+++ data_layer/api/db.py
@@ -1,6 +1,9 @@
 import datetime
+import json
 
 from flask_sqlalchemy import SQLAlchemy
+from flask import abort
+from .aqi import *
 
 db = SQLAlchemy()
 
@@ -15,3 +18,74 @@ class Station(db.Model):
 
     def __repr__(self):
         return '<station with id={}>'.format(self.id)
+
+
+def station_exists(id):
+    station = Station.query.get(id)
+    if station is None:
+        return False
+    return True
+
+
+def get_list_of_stations():
+    stations = Station.query.all()
+    return [
+        {
+            'id': station.id,
+            'latitude': station.latitude,
+            'longitude': station.longitude,
+            'last_txid': station.last_txid
+        }
+        for station in stations
+    ]
+
+
+def get_list_of_parameters():
+    stations = Station.query.all()
+    params = []
+    for station in stations:
+        for measurement in json.loads(station.data)['measurements']:
+            if not (measurement['parameter'] in params):
+                params.append(measurement['parameter'])
+    return [
+        parameter for parameter in params
+    ]
+
+
+def get_station_data(id):
+    station = Station.query.get(id)
+    if station is None:
+        abort(404)
+
+    return json.loads(station.data)
+
+
+def get_stations_data(aqi = False, parameters=[], coordinates = {}, limit = -1):
+    stations = Station.query.all()
+    if 'latitude' in coordinates.keys() and 'longitude' in coordinates.keys():
+        stations = sorted(stations,
+                        key=lambda station:
+                        pow(json.loads(station.data)['coordinates']['latitude'] - coordinates['latitude'], 2) +
+                        pow(json.loads(station.data)['coordinates']['longitude'] - coordinates['longitude'], 2))
+    data = []
+    for station in stations:
+        station_data = {
+            'id': station.id,
+            'latitude': station.latitude,
+            'longitude': station.longitude,
+            'last_txid': station.last_txid,
+        }   
+        measurements = [
+            measurement
+            for measurement in json.loads(station.data)['measurements']
+            if measurement['parameter'] in parameters or len(parameters) == 0
+        ]
+        if len(measurements) > 0:
+            station_data['measurements'] = measurements
+            if aqi:
+                station_data['aqi'] = get_aqi_of_station(measurements)
+            data.append(station_data)
+    
+    if limit >=0:
+        return data[:limit]
+    return data
